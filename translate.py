9962feeb91219c84df8f8bc36a4ff9635a5ad5a6
Maciej Gierada <maciek.gierada@gmail.com>
2021-05-28 17:24:16 +0200

    feat: add cloud translate feature

--- translate.py
+++ translate.py
@@ -24,6 +24,30 @@ class WordsTranslation():
                 is_correct = True
                 return random_words
 
+    def translate_text(self, target, text):
+        """Translates text into the target language.
 
-random_words = WordsTranslation().get_random_words()
-print(random_words)
+        Target must be an ISO 639-1 language code.
+        See https://g.co/cloud/translate/v2/translate-reference#supported_languages
+        """
+        import six
+        from google.cloud import translate_v2 as translate
+
+        translate_client = translate.Client()
+
+        if isinstance(text, six.binary_type):
+            text = text.decode("utf-8")
+
+        # Text can also be a sequence of strings, in which case this method
+        # will return a sequence of results for each text.
+        result = translate_client.translate(text, target_language=target)
+
+        print(u"Text: {}".format(result["input"]))
+        print(u"Translation: {}".format(result["translatedText"]))
+        print(u"Detected source language: {}".format(
+            result["detectedSourceLanguage"]))
+
+
+# random_words = WordsTranslation().get_random_words()
+WordsTranslation().translate_text('pl', 'play')
+# print(random_words)

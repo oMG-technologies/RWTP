fd5e3fe7a8681f60e37e1baec8537abaa324c5b7
Maciej Gierada <maciek.gierada@gmail.com>
2021-05-28 15:46:50 +0200

    feat: refactor and adding a method to return a list of 50 random words as from RandomWords().get_random_words() but making sure the request is 200

--- translate.py
+++ translate.py
@@ -1,20 +1,29 @@
 from random_word import RandomWords
 
 
-def get_random_words(n=10):
-    random_words = []
-    not_corrected_words = []
-    rw = RandomWords()
-    for _ in range(n):
-        is_correct_word = False
-        while is_correct_word is False:
-            word = rw.get_random_word(hasDictionaryDef=True)
-            not_corrected_words.append(word)
-            if word is not None:
-                is_correct_word = True
-                random_words.append(word)
-    return random_words
+class WordsTranslation():
+    def __init__(self):
+        self.rw = RandomWords()
+
+    def get_random_words_n(self, n=10):
+        random_words = []
+        for _ in range(n):
+            is_correct_word = False
+            while is_correct_word is False:
+                word = rw.get_random_word(hasDictionaryDef=True)
+                if word is not None:
+                    is_correct_word = True
+                    random_words.append(word)
+        return random_words
+
+    def get_random_words(self):
+        is_correct = False
+        while is_correct is False:
+            random_words = self.rw.get_random_words(hasDictionaryDef=True)
+            if random_words is not None:
+                is_correct = True
+                return random_words
 
 
-random_words = get_random_words()
+random_words = WordsTranslation().get_random_words()
 print(random_words)

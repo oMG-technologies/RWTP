e169441826b7ddb0bbd12b9773bf3a89120950c6
Maciej Gierada <maciek.gierada@gmail.com>
2021-05-29 22:24:56 +0200

    feat: added endpoint 'translations' to access all 50 translations (json response)

--- word_translation/api/serializers.py
+++ word_translation/api/serializers.py
@@ -5,5 +5,8 @@ from .models import Translation
 class TranslationSerializers(serializers.ModelSerializer):
     class Meta:
         model = Translation
-        fields = ['episode_number', 'date_published', 'link_to_mp3',
-                  'link_to_podcast', 'idd', 'status', 'text']
+        fields = [
+            'i',
+            'frontCard',
+            'backCard'
+        ]

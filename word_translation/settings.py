520473172fddf0e23a20043e29e6002952065622
Maciej Gierada <maciek.gierada@gmail.com>
2021-05-31 12:03:49 +0200

    fix: update settings

--- word_translation/settings.py
+++ word_translation/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-743muf%&f9j!9dxih@^d&zd*kth%*j6s*^!8utny5%f2bqwl@0'
 SECRET_KEY = os.environ['words_translation_secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!

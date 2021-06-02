7bb752803c30cb0f7a7cd6f1b655abd7f4df7dc8
Maciej Gierada <maciek.gierada@gmail.com>
2021-06-02 15:01:54 +0200

    fix: allow TranslationsViewSet to be accessed by unauthenticated users

--- api/views.py
+++ api/views.py
@@ -112,7 +112,7 @@ class TranslationsViewSet(viewsets.ModelViewSet):
     '''
     queryset = Translation.objects.all()
     serializer_class = TranslationSerializers
-    permission_classes = [permissions.IsAuthenticated]
+    # permission_classes = [permissions.IsAuthenticated]
 
 
 class UserViewSet(viewsets.ModelViewSet):

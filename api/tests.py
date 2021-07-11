1b8fea2c5de1f20ed5059a9818b9b92f3d6325c5
Maciej Gierada <maciek.gierada@gmail.com>
2021-07-11 16:14:21 +0200

    feat: adding tests to check endpoints

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+import requests
 
-# Create your tests here.
+
+class APIResponseTestCase(TestCase):
+    def test_tranlations_endpoint_response(self):
+        response = requests.get('http://127.0.0.1:8000/translations/')
+        assert response.status_code == 200
+
+    def test_signle_tranlation_endpoint_response(self):
+        languages_list = ['pl', 'de', 'fr', 'es', 'ru', 'it', 'sv', 'zh']
+        for language in languages_list:
+            response = requests.get(
+                'http://127.0.0.1:8000/translation/?conversion=en-{}/'.format(language))
+            assert response.status_code == 200
+
+    def test_all_single_translation_endpoint_content(self):
+        languages_list = ['pl', 'de', 'fr', 'es', 'ru', 'it', 'sv', 'zh']
+        for language in languages_list:
+            print(language)
+            response = requests.get(
+                'http://127.0.0.1:8000/translation/?conversion=en-{}/'.format(language))
+            print(response.text)
+            assert response.text != '[]'
+            print('{} test passed'.format(language))
+
+    def test_language_endpoint_response(self):
+        response = requests.get('http://127.0.0.1:8000/language/')
+        assert response.status_code == 200
+
+    def test_available_conversion_endpoint_response(self):
+        response = requests.get('http://127.0.0.1:8000/available_conversions/')
+        assert response.status_code == 200

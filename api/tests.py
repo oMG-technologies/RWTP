cede05a206e8082d00e3f48c5cff491060275866
Maciej Gierada <maciek.gierada@gmail.com>
2021-07-13 15:59:34 +0200

    fix: tests.py cleanup after development

--- api/tests.py
+++ api/tests.py
@@ -7,6 +7,14 @@ import json
 class APIResponseTestCase_01_GET(TestCase):
     print('# Testing GET requests #')
 
+    @property
+    def languages_list():
+        return ['pl', 'de', 'fr', 'es', 'ru', 'it', 'sv', 'zh']
+
+    @property
+    def single_language():
+        return ['pl']
+
     def test_tranlations_endpoint_response(self):
         print('\n  ### Testing \'/translations endpoint\'')
         response = requests.get('http://127.0.0.1:8000/translations/')
@@ -14,8 +22,8 @@ class APIResponseTestCase_01_GET(TestCase):
 
     def test_single_tranlation_endpoint_response(self):
         print('\n  ### Testing \'/translation/?conversion=en-x\' endpoint')
-        languages_list = ['pl', 'de', 'fr', 'es', 'ru', 'it', 'sv', 'zh']
-        for language in languages_list:
+
+        for language in self.languages_list:
             url = 'http://127.0.0.1:8000/translation/?conversion=en-{}'.format(
                 language)
             response = requests.get(url)
@@ -26,9 +34,8 @@ class APIResponseTestCase_01_GET(TestCase):
                 raise AssertionError
 
     def test_single_translation_endpoint_content(self):
-        # languages_list = ['pl', 'de', 'fr', 'es', 'ru', 'it', 'sv', 'zh']
-        languages_list = ['pl']
-        for language in languages_list:
+
+        for language in self.languages_list:
             url = 'http://127.0.0.1:8000/translation/?conversion=en-{}'.format(
                 language)
             response = requests.get(url)
@@ -39,9 +46,8 @@ class APIResponseTestCase_01_GET(TestCase):
                 raise AssertionError
 
     def test_single_translation_content_inner(self):
-        # languages_list = ['pl', 'de', 'fr', 'es', 'ru', 'it', 'sv', 'zh']
-        languages_list = ['pl']
-        for language in languages_list:
+
+        for language in self.languages_list:
             url = 'http://127.0.0.1:8000/translation/?conversion=en-{}'.format(
                 language)
             response = requests.get(url)
@@ -54,9 +60,7 @@ class APIResponseTestCase_01_GET(TestCase):
                     self.assertIn(key, inner_dict)
 
     def test_single_translation_content_inner_count(self):
-        # languages_list = ['pl', 'de', 'fr', 'es', 'ru', 'it', 'sv', 'zh']
-        languages_list = ['pl']
-        for language in languages_list:
+        for language in self.languages_list:
             url = 'http://127.0.0.1:8000/translation/?conversion=en-{}'.format(
                 language)
             response = requests.get(url)
@@ -182,15 +186,7 @@ class APIResponseTestCase_03_DELETE(TestCase):
         return os.environ['RWTP_su_passwd']
 
     def test_translation_endpoint_response_delete_authenticated(self):
-        print('delete')
-        # from api.models import Translation
         from requests.auth import HTTPBasicAuth
-
-        # translation_object = Translation.objects.all().filter(
-        #     frontCard__exact='some_word')
-
-        # current_id = translation_object[0].pk
-        # current_id = 32
 
         response = requests.get('http://127.0.0.1:8000/translations/')
         data = response.json()[-1]

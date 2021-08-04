814e5a885c3685f10048b91c232572c9ba1d5f9d
Maciej Gierada <maciek.gierada@gmail.com>
2021-08-04 17:06:33 +0200

    fix: remove unused code

--- api/views.py
+++ api/views.py
@@ -207,8 +207,6 @@ class UserCreateViewSet(UserViewSet):
 
         user = User.objects.create_user(
             username=username,
-            # first_name=first_name,
-            # last_name=last_name,
             email=email,
             password=password)
 
